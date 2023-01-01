785ab51c06fa0be1c022b04679794f82ad5ebbc6
gesshi <d.tcbn3+github@gmail.com>
2023-01-01 17:38:08 +0000

    footerをテンプレートで上書き

--- source/conf.py
+++ source/conf.py
@@ -7,14 +7,16 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'test-pages'
-copyright = '2023, gesshi'
+copyright = '2023 gesshi'
 author = 'gesshi'
 release = '0.0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    'myst_parser',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,6 +25,25 @@ language = 'ja'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_static_path = ['_static']
+html_css_files = [
+]
+
+html_context = {
+    'github_user': 'gesshi',
+    'github_repo': 'test-pages',
+}
 
 html_theme = 'furo'
-html_static_path = ['_static']
+html_theme_options = {
+    "footer_icons": [
+    ],
+}
+
+myst_enable_extensions = [
+    'colon_fence',
+    'tasklist',
+]
+
+myst_html_meta = {
+}
--- source/test.md
+++ source/test.md
@@ -1,9 +1,36 @@
-# MyST Test
+# My nifty title
 
-Hello, MyST.
+Some **text**!
 
-> Blockquote 
+```{admonition} Here's my title
+:class: warning
 
-1. spam
-1. ham
-1. eggs
+Here's my admonition content
+```
+
+(section-two)=
+## Here's another section
+
+And some more content.
+
+% This comment won't make it into the outputs!
+And here's {ref}`a reference to this section <section-two>`.
+I can also reference the section {ref}`section-two` without specifying my title.
+
+:::{note}
+And here's a note with a colon fence!
+:::
+
+And finally, here's a cool mermaid diagram!
+
+
+- [x] AAA
+- [ ] BBB
+- [ ] CCC
+
+```{math}
+   \begin{eqnarray}
+      y    & = & ax^2 + bx + c \\
+      f(x) & = & x^2 + 2xy + y^2
+   \end{eqnarray}
+```
